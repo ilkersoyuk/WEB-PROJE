21a52c68147ea8e7fcba8480180b16b15c7c8609
ilkersoyuk <ilker.soyuk@ogr.sakarya.edu.tr>
2025-05-16 22:40:08 +0300

    .

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ $password?$_POST['password'];
 
 if($username=="b241210059@gmail.com" && $password=="b241210059") 
 {
-    echo "Şifre Doğru Yönlendiriliyorsunuz";
+    echo "Şifre Doğru Hoşgeldiniz 'b241210059' ";
 }
 
 else{
