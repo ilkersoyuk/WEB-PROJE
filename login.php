73067dafba13bbbedd3cada66388e7bb718b2ae5
ilkersoyuk <ilker.soyuk@ogr.sakarya.edu.tr>
2025-05-17 20:12:43 +0300

    .

--- login.php
+++ login.php
@@ -1,16 +1,18 @@
 <?php
 
 $username=$_POST['username'];
-$password?$_POST['password'];
+$password=$_POST['password'];
 
-if($username=="b241210059@gmail.com" && $password=="b241210059") 
+if($username=="b241210059@sakarya.edu.tr" && $password=="b241210059") 
 {
+    header("Location: hakkimda.html");
+
     echo "Şifre Doğru Hoşgeldiniz 'b241210059' ";
 }
 
 else{
     header("Location:login.php?u=$username");
-    echo "Hatalı Gİriş Yaptınız Tekrar Deneyiniz."
+    echo "Hatalı Gİriş Yaptınız Tekrar Deneyiniz.";
 }
 
 
