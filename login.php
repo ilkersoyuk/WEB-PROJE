04b66cee9fee64a5f8b12e3e0ad50d3a702d9d26
ilkersoyuk <ilker.soyuk@ogr.sakarya.edu.tr>
2025-05-19 16:49:33 +0300

    .

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = trim($_POST['gelen_email'] ?? '');
+    $password = trim($_POST['gelen_password'] ?? '');
+
+    if ($email == 'b241210059@sakarya.edu.tr' && $password == 'b241210059') {
+        echo "Hoşgeldiniz b241210059";
+        echo "<script>setTimeout(function(){ window.location.href = 'hakkimda.html'; }, 2000);</script>";
+        exit();
+    } 
+    else {
+        echo "<script>setTimeout(function(){ window.location.href = 'login.html'; }, 2000);</script>";
+        echo "<p style='color:red;'>Kullanıcı adı veya şifre hatalı. Tekrar deneyiniz.</p>";
+    }
+}
+?>
